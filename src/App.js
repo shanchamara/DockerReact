fa723cc9920c6f60293c0a849633f19be1223507
My Name <86416827+shanchamara@users.noreply.github.com>
2024-11-15 11:41:09 +0530

    first commit12

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Madushan
         </p>
         <a
           className="App-link"
