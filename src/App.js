bf547ff59faa0a3dfb8784c702b2111981b6de30
My Name <86416827+shanchamara@users.noreply.github.com>
2024-11-15 11:51:16 +0530

    first commit1212

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Madushan app
+          Madushan app nn
         </p>
         <a
           className="App-link"
